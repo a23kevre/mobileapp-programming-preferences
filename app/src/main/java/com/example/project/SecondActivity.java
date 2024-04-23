8af30104f8d183a9a8d8ebcbac97080d89f80558
a23kevre <a23kevre@student.his.se>
2024-04-23 09:54:00 +0200

    fungerande gradle, med två aktiviterer skapade.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
