85e2313599ba4293ca09c974dcc526bc8c55c5b3
a23kevre <a23kevre@student.his.se>
2024-04-23 10:23:51 +0200

    skapat en knapp som startar andra aktiviteten från den första aktiviteten.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,7 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Button b = findViewById(R.id.myBtn);
+        b.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d("MMM", "bytte sida");
+                Intent second = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(second);
+            }
+        });
     }
 }
