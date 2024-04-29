38547d63ae5b0088e2712411e676a6ade97bad18
a23kevre <a23kevre@student.his.se>
2024-04-29 09:37:57 +0200

    gjort så att shared preference visas på MainActivity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +22,21 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Log.d("MMM", "bytte sida");
+                Log.d("MMM", "new activity");
                 Intent second = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(second);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myPreferenceRef = getSharedPreferences("MyPreferenceName", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+        TextView prefTextRef=new TextView(this);
+        Log.d("VVV", ""+myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
     }
 }
